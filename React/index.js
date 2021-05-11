212cb23a56f81b0616b8425a2e44485e761faa9b
sh414lim <77113306+sh414lim@users.noreply.github.com>
2021-05-11 09:18:16 +0900

    .

--- React/index.js
+++ React/index.js
@@ -1,43 +1,32 @@
 class ObjectUtilities {
-    static mergeObjects(a, b) {
-      return { ...a, ...b };
-    }
-    static removePassword() {
-      return delete user.password;
-    }
-    static freezeObj() {
-      return Object.freeze(cleanUser);
-    }
-    static getOnlyValues() {
-      return Object.values(user);
-    }
-    static getOnlyProperties() {
-      return Object.getOwnPropertyNames(user);
-    }
-  }
-  
-  const objA = {
-    name: "Nicolas",
-    favFood: "Kimchi"
-  };
-  
-  const objB = {
-    password: "12345"
-  };
-  
-  const user = ObjectUtilities.mergeObjects(objA, objB);
-  console.log(user);
-  
-  const cleanUser = ObjectUtilities.removePassword(user);
-  console.log(cleanUser);
-  
-  const frozenUser = ObjectUtilities.freezeObj(cleanUser);
-  
-  const onlyValues = ObjectUtilities.getOnlyValues(frozenUser);
-  console.log(onlyValues);
-  
-  const onlyProperties = ObjectUtilities.getOnlyProperties(frozenUser);
-  console.log(onlyProperties);
-  
-  frozenUser.name = "Hello!"; // This should show an error
-  
+  static mergeObjects = (objectA, objectB) => ({ ...objectA, ...objectB });
+  static removePassword = ({ password, ...rest }) => rest;
+  static getOnlyProperties = obj => Object.keys(obj);
+  static getOnlyValues = obj => Object.values(obj);
+  static freezeObj = obj => Object.freeze(obj);
+}
+
+const objA = {
+  name: "Nicolas",
+  favFood: "Kimchi"
+};
+
+const objB = {
+  password: "12345"
+};
+
+const user = ObjectUtilities.mergeObjects(objA, objB);
+console.log(user);
+
+const cleanUser = ObjectUtilities.removePassword(user);
+console.log(cleanUser);
+
+const frozenUser = ObjectUtilities.freezeObj(cleanUser);
+
+const onlyValues = ObjectUtilities.getOnlyValues(frozenUser);
+console.log(onlyValues);
+
+const onlyProperties = ObjectUtilities.getOnlyProperties(frozenUser);
+console.log(onlyProperties);
+
+frozenUser.name = "Hello!"; // This should show an error
